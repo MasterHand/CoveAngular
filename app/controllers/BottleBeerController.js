4bd1a08666b250c873459a1806c2a3bf38e0b8e8
Andrew Bergeron <abergeron@infiniteinnovationsllc.com>
2014-06-29 18:00:47 -0500

    Array scope issues. Able to successfully place a temporary document into a temporary array and display it. Attempting to make available to $scope.

--- app/controllers/BottleBeerController.js
+++ app/controllers/BottleBeerController.js
@@ -4,7 +4,7 @@
         $scope.sortBy = 'name';
         $scope.reverse = false;
         $scope.json = [];
-        $scope.bottleBeers = [];
+        $scope.bottleBeers = new Array();
         $scope.appSettings = appSettings;
 
         function init() {
@@ -12,29 +12,26 @@
                 .success(function(json, bottleBeers) {
                     $log.log("HTTP REQUEST SUCCESSFUL");
 
-                    $log.log("TOTAL DOCS: " + (json.total_rows));
+                        // $log.log("TOTAL DOCS: " + (json.total_rows));
 
-                    var tDoc = {};   // Temp obj to hold CouchDB doc
+                    var tDoc = {};   // Temp Object to hold CouchDB doc
+                    var tArray = new Array();
+                    // Attempt to place first document into bottleBeers Array
+                    tDoc = json.rows[0].doc;
+                    tArray.push(tDoc);
+                    $log.log(tArray[0]);
 
-                    $log.log("ATTEMPTING TO DISPLAY ALL TEMP OBJECTS");
+                    /*
                     for (var n = 0; n < json.total_rows; n++ ) {
                         tDoc = json.rows[n].doc;
-                        $log.log(tDoc);
-                    }
+                    }*/
 
-                    /* THE FOLLOWING CODE WORKS:
-                    tDoc = json.rows[0].doc;
-                    $log.log("ATTEMPTING TO DISPLAY TEMP OBJ");
-                    $log.log(tDoc);
-                    END */
-
-                    /* THE FOLLOWING CODE WORKS:
-                    $log.log("ATTEMPTING TO DISPLAY SOMETHING");
-                    $log.log(json.rows[0].doc);
-                    END */
-
-                    // $log.log("DEBUG: Use Angular to Iterate through results");
-
+                    /*
+                    $log.log("LOGGING ALL BOTTLE BEERS BELOW:");
+                    for (var t = 0; t < bottleBeers.length; t++) {
+                        // Log bottleBeers
+                        $log.log(bottleBeers[t]);
+                    }*/
 
                 })
                 .error(function(data, status, headers, config) {
@@ -44,7 +41,6 @@
         }
 
         init();
-
 
     };
     
