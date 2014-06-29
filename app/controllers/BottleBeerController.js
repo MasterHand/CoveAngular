ea2029f5fe5c5d75fbea02bc7d5580092c69be0b
Andrew Bergeron <abergeron@infiniteinnovationsllc.com>
2014-06-29 15:37:21 -0500

    stored json object in a reference to bottleBeer, returns its length and offset

--- app/controllers/BottleBeerController.js
+++ app/controllers/BottleBeerController.js
@@ -1,29 +1,41 @@
 (function() {
-    
+
     var BottleBeerController = function ($scope, $log, BottleBeerFactory, appSettings) {
         $scope.sortBy = 'name';
         $scope.reverse = false;
-        $scope.bottleBeers = [];
+        //new
+        $scope.bottleBeers= {content:null};
         $scope.appSettings = appSettings;
-        
+
         function init() {
             BottleBeerFactory.getBottleBeer()
                 .success(function(bottleBeers) {
                     $log.log("HTTP REQUEST SUCCESSFUL");
 
+                    //new
+                    $scope.bottleBeers.content = bottleBeers;
+                    //return bottleBeers;
+
+
                 })
                 .error(function(data, status, headers, config) {
                     $log.log(data.error + ' ' + status);
                 });
+            //new
+           // return bottleBeers;
         }
-        
+
         init();
-        
-        $scope.doSort = function(pName) {
-           $scope.sortBy = pName;
-           $scope.reverse = !$scope.reverse;
-        };
+
+
     };
+
+
+
+
+
+
+
     
     BottleBeerController.$inject = ['$scope', '$log', 'BottleBeerFactory',
                                    'appSettings'];
