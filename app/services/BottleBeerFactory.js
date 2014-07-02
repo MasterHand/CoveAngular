f9fc8d406cc81836441609114270b93537f39609
MasterHand <abergeron@infiniteinnovationsllc.com>
2014-07-01 21:48:27 -0500

    Modified BottleBeerFactory to include Base64 object for HTTP Basic Auth. Successfully requests JSON data from Cloudant URL.

--- app/services/BottleBeerFactory.js
+++ app/services/BottleBeerFactory.js
@@ -1,20 +1,40 @@
 (function() {
 
-    var BottleBeerURL = 'https://infiniteinnovations.cloudant.com/covebeta/_design/drink/_view/BottleBeer?include_docs=true&reduce=false';
-
     var BottleBeerFactory = function($http) {
 
         var factory = {};
-
+        /*
         factory.getBottleBeer = function () {
 
            return $http.get('app/db/BottleBeer.json');
         };
+        */
 
+        $http.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+        factory.getBottleBeer = function () {
+
+            var user = "commeelyinguederistroust";
+            var pass = "2imaJ47lISdfKTXV48oAKdXp";
+
+            var db = "covebeta/_design/drink/_view/BottleBeer?include_docs=true&reduce=false";
+
+            var beerURL = "https://" + user + ":" + pass + "@" + "infiniteinnovations.cloudant.com/" + db;
+
+            // Request Headers
+            //$http.defaults.headers.request['Content-type'] = 'application/json';
+
+            //$http.defaults.headers.request['Accept'] = 'application/json';
+
+            // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+
+            return $http.get(beerURL);
+        };
 
         return factory;
     };
-    
+
     BottleBeerFactory.$inject = ['$http'];
         
     angular.module('coveApp').factory('BottleBeerFactory',
