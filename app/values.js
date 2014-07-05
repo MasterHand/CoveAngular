52c6cbce32b244ee2fc1338a6533ada5fe3a6b16
Maurice <maurice@infiniteinnovationsllc.com>
2014-07-05 00:56:31 -0500

    Added styled table to each view. Ready to apply more advanced styling.

--- app/values.js
+++ app/values.js
@@ -1,5 +1,5 @@
 angular.module('coveApp').value('appSettings', {
-    title: 'Cove App Admin',
+    title: 'Cove App Administration',
     version: '0.5',
     devLocal: true     // Set this to true in order to use JSON files in db folder
 
